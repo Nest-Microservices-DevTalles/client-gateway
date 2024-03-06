0780d45f2998fac1ee8fbd50179033f53bf87489
Fernando Herrera <fernando.herrera85@gmail.com>
2024-03-06 12:13:44 -0500

    Add console log statement - Hola Mundo - Primer cambio

--- src/main.ts
+++ src/main.ts
@@ -21,6 +21,8 @@ async function bootstrap() {
 
   await app.listen(envs.port);
 
+  console.log('Hola Mundo - Primer cambio');
+
   logger.log(`Gateway running on port ${envs.port}`);
 }
 bootstrap();
